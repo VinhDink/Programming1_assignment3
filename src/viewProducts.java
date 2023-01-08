cbfeee31f1da5576df6f5055611934a058ee3dd1
VinhDink <97300561+VinhDink@users.noreply.github.com>
2023-01-08 18:38:47 +0700

    Fix main

--- src/viewProducts.java
+++ src/viewProducts.java
@@ -11,9 +11,13 @@ public class viewProducts extends Product {
     }
 
     public void viewProdsInfo() {
+        System.out.printf("%-15s%-30s%-10s%-15s%-10s\n", "ID", "Title", "Price", "Category", "In Stock");
+        System.out.println("----------------------------------------------------------------------------");
         for (int i = 0; i < getItems_data().size();i++)
         {
-            System.out.println("Item "+i+": " +  getItems_data().get(i));
+            String[] arr = getItems_data().get(i).split(",");
+            System.out.printf("%-15s%-30s%-10s%-15s%-10s\n",
+                    arr[0], arr[1], arr[2], arr[3], arr[4]);
         }
     }
 }
