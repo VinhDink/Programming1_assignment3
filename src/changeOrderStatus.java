f3b67a529a94a4c5d895c999885369b564df56af
MNht12 <s3924871@rmit.edu.vn>
2023-01-12 17:57:47 +0700

    Fix bugs, check for input parameter.

--- src/changeOrderStatus.java
+++ src/changeOrderStatus.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class changeOrderStatus extends Order {
@@ -9,6 +10,7 @@ public class changeOrderStatus extends Order {
     public void changeStatus() {
         ArrayList<String> id_arr = new ArrayList<String>();
         boolean valid = false;
+        boolean correct = false;
         Scanner scanner = new Scanner(System.in);
         for (int i = 0; i < getOrders_data().size(); i++) {
             String[] arr = getOrders_data().get(i).split(",");
@@ -24,16 +26,27 @@ public class changeOrderStatus extends Order {
             else {
                 int i = id_arr.indexOf(ID);
                 String[] arr = getOrders_data().get(i).split(",");
-                System.out.println("Change status of order to:" +
+                while (!correct) {
+//                    int opt = 0;
+                    try {
+                        System.out.println("Change status of order to:" +
                                 "\n1 paid" +
                                 "\n2 unpaid");
-                int opt = scanner.nextInt();
-                if(opt == 1) {
-                    arr[8] = "paid";
-                } else if (opt == 2) {
-                    arr[8] = "unpaid";
-                } else {
-                    System.out.println("Input invalid!");
+                        int opt = scanner.nextInt();
+                        if(opt == 1) {
+                            arr[8] = "paid";
+                            correct = true;
+                        } else if (opt == 2) {
+                            arr[8] = "unpaid";
+                            correct = true;
+                        } else {
+                            System.out.println("Input invalid!");
+                        }
+                    } catch (InputMismatchException e) {
+                        scanner.nextLine();
+                        System.out.println("Invalid parameter!");
+                    }
+
                 }
                 String new_data = arr[0]+","+arr[1]+","+arr[2]+","+arr[3]+","+arr[4]+","+arr[5]+","+arr[6]+","+arr[7]+","+arr[8];
                 getOrders_data().set(i,new_data);
